fffb03cf13b8b5b8c68cf225ed0bc7ef83b47eff
V415hT313 <vashu2506rkt@gmail.com>
2026-08-31 15:43:02 +0530

    postgres setup with docker image

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,16 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+load_dotenv()
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+SQLALCHEMY_DATABASE_URL = os.environ.get(
+    "DATABASE_URL", "postgresql+psycopg://appuser:apppassword@localhost:5432/aiproject"
 )
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
